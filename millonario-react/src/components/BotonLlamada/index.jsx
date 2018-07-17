8df0e8024c39380999c1a9e0d839993f05777632
LeoPindjs <leoapineda@gmail.com>
2018-07-16 21:30:42 -0500

    Cración del componente BotonLlamada

---
+++ millonario-react/src/components/BotonLlamada/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BotonLlamada = () => (
+    <button id="btn__llamada" class="btn__comodin">
+        <i class="fa fa-phone fa-3x" aria-hidden="true"></i>
+    </button>
+)
+
+export default BotonLlamada
